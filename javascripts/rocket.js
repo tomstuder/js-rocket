0d0bc741b4338513246e680a662747d31e645277
tomstuder <tom@tomstuder.com>
2016-04-23 20:17:53 -0400

    adding js

--- javascripts/rocket.js
+++ javascripts/rocket.js
@@ -13,12 +13,47 @@ var boom = function () {
 
 // ROCKET LAUNCH PAGE --------------------------------------------
 
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+  document.body.className = 'body-state'+state;
+  clearInterval(timer);
+  countdownNumber = 10;
+  document.getElementById('countdown').innerHTML = countdownNumber;
+
+//   // countdown
+//   if (state == 2) {
+//     timer = setInterval(function () {
+//       countdownNumber = countdownNumber-1;
+//       document.getElementById('timer').innerHTML = countdownNumber;
+
+//       if (countdownNumber <= 0) {
+//         changeState(3);
+//       };
+//     }, 500);
+//   } else if (state == 3) {
+//     var success = setTimeout(function () {
+//         var randomNumber = Math.round(Math.random()*10);
+
+//         console.log('randomNumber:', randomNumber)
+
+//         // succes
+//         if (randomNumber > 2) {
+//           changeState(4);
+
+//         } else {
+//           changeState(5); // BOOOOOM!
+//         }
+//     }, 2000);
+//   };
+// }
+
+
 
 // Launch Rocket
 var launch = function () {
   document.getElementById('rocket').className = 'rocket launch';
-  document.getElementById('test').style.display = 'block';
-  document.getElementById('test').style.backgroundColor = 'red';
 }
 
 // Reset the Rocket Launch
