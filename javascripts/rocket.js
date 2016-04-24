6659d1c1dce1aafa36d9967efa54b9e60216386a
tomstuder <tom@tomstuder.com>
2016-04-23 22:30:25 -0400

    refined and added js

--- javascripts/rocket.js
+++ javascripts/rocket.js
@@ -11,57 +11,41 @@ var boom = function () {
   }
 }
 
-// ROCKET LAUNCH PAGE --------------------------------------------
+// ROCKET LAUNCHER JS
 
-// var timer = null;
-// var countdownNumber = 10;
+var timer = null;
+var countdownNumber = 10;
 
-// var changeState = function (state) {
-//   document.body.className = 'body-state'+state;
-//   clearInterval(timer);
-//   countdownNumber = 10;
-//   document.getElementById('countdown').innerHTML = countdownNumber;
+var changeState = function (state) {
+  document.body.className = 'body-state'+state;
+  clearInterval(timer);
+  countdownNumber = 10;
+  document.getElementById('countdown').innerHTML = countdownNumber;
 
-//   // countdown
-//   if (state == 2) {
-//     timer = setInterval(function () {
-//       countdownNumber = countdownNumber-1;
-//       document.getElementById('timer').innerHTML = countdownNumber;
+  // countdown
+  if (state == 2) {
+    timer = setInterval(function () {
+      countdownNumber = countdownNumber-1;
+      document.getElementById('countdown').innerHTML = countdownNumber;
+      
+      if (countdownNumber <= 0) {
+        changeState(3);
+      };
 
-//       if (countdownNumber <= 0) {
-//         changeState(3);
-//       };
-//     }, 500);
-//   } else if (state == 3) {
-//     var success = setTimeout(function () {
-//         var randomNumber = Math.round(Math.random()*10);
+    }, 500);
+  } else if (state == 3) {
+    var success = setTimeout(function () {
+        var randomNumber = Math.round(Math.random()*10);
 
-//         console.log('randomNumber:', randomNumber)
+        console.log('randomNumber:', randomNumber)
 
-//         // succes
-//         if (randomNumber > 2) {
-//           changeState(4);
+        // successful launch
+        if (randomNumber > 4) {
+          changeState(4);
 
-//         } else {
-//           changeState(5); // BOOOOOM!
-//         }
-//     }, 2000);
-//   };
-// }
-
-
-
-// Launch Rocket
-var launch = function () {
-  document.getElementById('rocket').className = 'rocket launch';
-}
-
-// Reset the Rocket Launch
-var relaunch = function () {
-  console.log('relaunch rocket')
-}
-
-// Stop the Rocket Launch
-var abortlaunch = function () {
-  console.log('abort launch')
+        } else {
+          changeState(5); // Boom!
+        }
+    }, 2000);
+  };
 }
